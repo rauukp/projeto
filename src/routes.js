bdd97fec9fb9c20e06c323b77c4703ffb6f2509c
rauukp <raul_lopes11k@outlook.com>
2024-02-28 22:24:17 -0300

    main page added

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const routes = express()
 
-
+routes.get('/', async (req,res) => {
+    try {
+        const allUsers = await knex('usuarios');
+        return res.json(allUsers);
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'erro no servidor' })
+    }
+})
 
 module.exports = {
     routes
