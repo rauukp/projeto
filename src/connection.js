1affc2ce590a983aa87ca53432902e76ad206812
rauukp <raul_lopes11k@outlook.com>
2024-02-28 22:07:30 -0300

    configurado db

--- src/connection.js
+++ src/connection.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        port: 5432,
-        user: 'postgres',
-        password: '<PASSWORD>',
-        database: 'dindin',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         ssl: { rejectUnauthorized: false}
     }
 });
