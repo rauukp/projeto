dd84e2dad42b1270859aec4c07279b98f4ed32a9
rauukp <raul_lopes11k@outlook.com>
2024-02-28 23:29:05 -0300

    fix error db connection

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -3,25 +3,26 @@ const knex = require('../connection')
 const jwt = require('jsonwebtoken')
 
 const newUser = async (req,res) => {
-    const { nome, email, pass } = req.body;
+    const { nome, email, senha } = req.body;
 
     if (!nome) {
         return res.status(400).json({"mensagem": "Informe um nome valido para cadastro."})
     } if (!email) {
         return res.status(400).json({"mensagem": "Informe um email valido para cadastro."})
-    } if (!pass) {
+    } if (!senha) {
         return res.status(400).json({"mensagem": "Informe uma senha valida para cadastro."})
     };
 
     const newU = {
         nome,
         email,
-        pass
+        senha
     }
     try {
         const validUser = await knex('usuarios').insert(newU).returning(['id','nome']);
         return res.status(201).json(validUser);
     } catch (error) {
+        console.log(error.message);
         return res.status(500).json({ mensagem: 'erro no servidor' })
     }
 };
